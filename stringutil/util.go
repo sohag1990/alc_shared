6dbd1e7336fdd19c58d41fe1f0f8dbf973dfb88b
Shakhawat Hossain <whoshakhawat@gmail.com>
2023-05-19 11:40:17 +0600

    Stringutil add

---
+++ stringutil/util.go
@@ -0,0 +1,18 @@
+// util.go
+package stringutil
+
+import "strings"
+
+// ReverseString reverses a given string.
+func ReverseString(s string) string {
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return string(runes)
+}
+
+// ToUpperCase converts a string to uppercase.
+func ToUpperCase(s string) string {
+	return strings.ToUpper(s)
+}
