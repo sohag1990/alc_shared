8e40574274541d1551ee4428c7c74285e636d8ee
Shakhawat Hossain <whoshakhawat@gmail.com>
2023-05-21 10:46:22 +0600

    jwt helpers remove

--- alc_shared.go
+++ alc_shared.go
@@ -5,7 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-var API_URL = "http://localhost:8080/api"
+// var API_URL = "http://localhost:8080/api"
 
 // Cors set cors to routes
 func Cors() gin.HandlerFunc {
