4db4000a9e4a9c51e97139226cdafd5e67dbace6
Shakhawat Hossain <whoshakhawat@gmail.com>
2024-08-22 22:29:08 +0600

    PAT Cert add

--- models/certificate_models.go
+++ models/certificate_models.go
@@ -39,6 +39,25 @@ type EmergencyLightingCert struct {
 	RelatedReference  string `gorm:"size:550"`
 	Validity          string `gorm:"size:50"`
 }
+
+type PatCert struct {
+	DefaultProperties
+	OrderID  uint64
+	Name     string `gorm:"size:100"`
+	Email    string `gorm:"size:100"`
+	Address  string `gorm:"size:250"`
+	PostCode string `gorm:"size:20"`
+	Phone    string `gorm:"size:20"`
+
+	LandlordName     string    `gorm:"size:100"`
+	LandlordAddress  string    `gorm:"size:250"`
+	LandlordPostCode string    `gorm:"size:20"`
+	AssessedBy       string    `gorm:"size:100"`
+	Registration     string    `gorm:"size:100"`
+	AssessmentDate   time.Time `form:"AssessmentDate"  time_format:"2006-01-02"`
+	ReviewDate       time.Time `form:"ReviewDate"  time_format:"2006-01-02"`
+}
+
 type FireAlarmCert struct {
 	DefaultProperties
 	OrderID  uint64
